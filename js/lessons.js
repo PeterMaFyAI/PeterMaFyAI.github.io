e2e8457fa8f59fe52e2f6ac98bc8ea02529c40da
PeterMaFyAI <peter.navert@outlook.com>
2024-12-29 10:40:09 +0100

    Update lessons.js

--- js/lessons.js
+++ js/lessons.js
@@ -9,13 +9,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const basePath = 'data/';
     const courses = {
-        "Fysik 1": { chapters: 12, path: 'fysik1' },
-        "Fysik 2": { chapters: 8, path: 'fysik2' }
-    };
-
-    const urlToCourseMapping = {
-        "fysik1": "Fysik 1",
-        "fysik2": "Fysik 2"
+        "fysik1": { chapters: 12, path: 'fysik1' },
+        "fysik2": { chapters: 8, path: 'fysik2' }
     };
 
     // Function to create the main menu
